b7b2fb4a121fd0b3f423c76d3ab01036c672f2b5
Bradley Davy <bd67@login01.hpc.st-andrews.ac.uk>
2025-03-17 10:51:28 +0000

    For now, output is saved to a csv file. The long term plan is to save this data to a sql database. I can only do this when I know which server to run this on. I can use to the csv files as a template before I have my sql tables working.

--- slurm_comunicator/main.py
+++ slurm_comunicator/main.py
@@ -1,8 +1,9 @@
 
-from slurm_comunicator.comunicator import SlurmComms  # Class names should be PascalCase
+from slurm_comunicator.comunicator import SlurmComms  
 from slurm_comunicator.utils import *
 import numpy as np
 import shutil
+from datetime import date
 
 def add(x,y):
     return x+y
@@ -20,19 +21,24 @@ if __name__ == '__main__':
 
     terminal_width = shutil.get_terminal_size().columns
     terminal_height = shutil.get_terminal_size().lines
+    quantities_of_interest = {'date': date.today(), 'number_of_cores' : 0, 'average_length_of_job' : 0, 'jobs_in_the_queue' : 0,
+                              '' : 0, 'large-long' : 0, 'large-sho+' : 0, 'small-long' : 0, 'interacti+' : 0}
 
     print_new_section('Slurm Comunicator', terminal_width)
 
     comms = SlurmComms()
 
-    number_of_cores_check = True
-    average_length_of_job_check = True
-    check_partition_useage = True
+    number_of_cores_check = False
+    average_length_of_job_check = False
+    check_partition_useage = False
+    print_csv_file_check = True
+    append_to_csv_check = True
 
     if number_of_cores_check == True:
         ## Get statistics on the number of cores being used ##
         total_cores_used = comms.get_total_number_of_cores_in_use()
         print(f'Total cores used: {total_cores_used}, {100*(total_cores_used/15102):.2f}% of the HPC is currently being used.')
+        quantities_of_interest['number_of_cores'] = total_cores_used
 
     if average_length_of_job_check == True:
         ## Determine the average length of time of a job ##
@@ -45,6 +51,7 @@ if __name__ == '__main__':
         average_requested_time = np.average(requested_time)
         print(f'Average requested time over last 24 hours: {convert_minuets_in_to_time_string(average_requested_time)}')
         print(f'Average run time over last 24 hours: {convert_minuets_in_to_time_string(average_run_time)}')
+        quantities_of_interest['average_length_of_job'] = average_run_time
 
     if check_partition_useage == True:
         ## Determine the amounts of jobs currently running in each parition ##
@@ -61,9 +68,24 @@ if __name__ == '__main__':
             else:
                 number_of_cores_per_partition[partition].append(number_of_cores)
         
+        print(number_of_cores_per_partition)
+        print('\n')
         for partition in number_of_cores_per_partition.keys():
-            print(f'Partition: {partition}, {len(number_of_cores_per_partition[partition])} jobs currently running, {sum(number_of_cores_per_partition[partition])} cores in use.')
 
+            number_of_running_jobs = 0
+            total_cores_used = 0
+            for cores in number_of_cores_per_partition[partition]:
+                if cores > 0:
+                    number_of_running_jobs += 1
+                    total_cores_used += cores
+            quantities_of_interest[partition] = number_of_running_jobs
+            print(f'''Partition: {partition if partition != '' else 'Queue'}, {number_of_running_jobs}
+                   jobs currently running, {sum(number_of_cores_per_partition[partition])} cores in use.
+                   Average number of cores per job: {total_cores_used / number_of_running_jobs if number_of_running_jobs != 0 else 0:.0f}.''')
+        print('\n')
 
+    if append_to_csv_check == True:
+        append_to_csv(quantities_of_interest)
 
-        
+    if print_csv_file_check == True:
+        print_csv_file()
--- slurm_comunicator/utils.py
+++ slurm_comunicator/utils.py
@@ -2,6 +2,21 @@ from slurm_comunicator.comunicator import SlurmComms
 import pandas as pd
 from datetime import date
 
+def create_csv_file(data: dict) -> None:
+    csv_file_path = '/home/bd67/scratch/hypatia_logs/slurm_comunicator.csv'
+    df = pd.DataFrame(data, index=[0])
+    df.to_csv(csv_file_path, index=False)
+
+def append_to_csv(input_data: dict) -> None:
+    csv_file_path = '/home/bd67/scratch/hypatia_logs/slurm_comunicator.csv'
+    df = pd.DataFrame(input_data, index=[0])
+    df.to_csv(csv_file_path, mode='a', header=False, index=False)
+
+def print_csv_file() -> None:
+    csv_file_path = '/home/bd67/scratch/hypatia_logs/slurm_comunicator.csv'
+    df = pd.read_csv(csv_file_path)
+    print(df)
+
 def save_array_as_excel(input_array: list, file_name: str = 'output') -> None:
     df = pd.DataFrame(input_array)
     df.to_excel(f'{file_name}_{date.today()}.xlsx', index=False)
