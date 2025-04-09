ddc15070695387260ed0fc18f3918d61f47462c2
Bradley Davy <bd67@login01.hpc.st-andrews.ac.uk>
2025-04-09 14:39:56 +0100

    Added a test.

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,6 @@ import subprocess
 
 
 ## Tests SlurmComms class ##
-def partition_cores_equals_total_cores():
+def test_partition_cores_equals_total_cores():
     comms = SlurmComms()
     assert sum(comms.get_n_cores_partition_dictionary().values()) == comms.get_total_cores_in_use()
